682409edd9e2fceae628a17b48137d5bbc755606
JojiKoike <joji.koike@gmail.com>
2021-06-06 20:56:05 +0900

    :+1: arrange layout

--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -5,15 +5,13 @@ import { Layout } from "../components";
 
 const ThanksPage: React.FC<PageProps> = ({ location, ...props }) => (
   <Layout location={location}>
-    <div className="container bg-white p-6 mb-2 max-w-6xl mx-auto rounded-md shadow-md">
-      <h1 className="text-center text-lg sm:text-3xl">Thanks</h1>
+    <div className="container text-center bg-white p-6 mb-2 max-w-6xl mx-auto rounded-md shadow-md">
+      <h1 className="text-lg sm:text-3xl">Thanks</h1>
       <p>メッセージ送信ありがとうございました。</p>
 
-      <div className="flex justify-center sm:justify-end">
-        <Link to={`/contact`} rel="contact">
-          Contactへ戻る
-        </Link>
-      </div>
+      <Link to={`/contact`} rel="contact">
+        Contactへ戻る
+      </Link>
     </div>
   </Layout>
 );
