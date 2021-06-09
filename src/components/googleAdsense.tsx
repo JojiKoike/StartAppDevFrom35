ec277a334c08efc70d9f8a63cf8feae00f616282
JojiKoike <joji.koike@gmail.com>
2021-06-09 23:59:16 +0900

    :shower: Removed Unnecessary Code

--- src/components/googleAdsense.tsx
+++ src/components/googleAdsense.tsx
@@ -2,18 +2,15 @@ import React from "react";
 import Adsense from "react-adsense";
 
 const HorizontalBarAd: React.FC = () => (
-  <>
-    <ins className="adsbygoogle">
-      <Adsense.Google
-        style={{ display: "block" }}
-        client="ca-pub-3835939635569436"
-        slot="1367382719"
-        format="auto"
-        responsive="true"
-      />
-    </ins>
-    <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-  </>
+  <ins className="adsbygoogle">
+    <Adsense.Google
+      style={{ display: "block" }}
+      client="ca-pub-3835939635569436"
+      slot="1367382719"
+      format="auto"
+      responsive="true"
+    />
+  </ins>
 );
 
 export default HorizontalBarAd;
