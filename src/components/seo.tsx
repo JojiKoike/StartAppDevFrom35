5ae1d1b85616125baca256d283f095acc7f90fd1
JojiKoike <joji.koike@gmail.com>
2021-06-10 08:53:28 +0900

    :+1: Auto Ads Setting

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -123,6 +123,12 @@ const Seo: React.FC<SeoProps> = ({
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
       ></script>
+      <script>
+        {`(adsbygoogle = window.adsbygoogle || []).push({
+           google_ad_client: "ca-pub-3835939635569436",
+           enable_page_level_ads: true
+        });`}
+      </script>
     </Helmet>
   );
 };
