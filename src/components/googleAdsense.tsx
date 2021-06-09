d1e23233b7a4abe4e88cc448b2d80b20e2c4a7eb
JojiKoike <joji.koike@gmail.com>
2021-06-09 23:31:10 +0900

    :+1: Add Script

--- src/components/googleAdsense.tsx
+++ src/components/googleAdsense.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import Adsense from "react-adsense";
 
 const HorizontalBarAd: React.FC = () => (
-  <ins className="adsbygoogle">
-    <Adsense.Google
-      style={{ display: "block" }}
-      client="ca-pub-3835939635569436"
-      slot="1367382719"
-      format="auto"
-      responsive="true"
-    />
-  </ins>
+  <>
+    <ins className="adsbygoogle">
+      <Adsense.Google
+        style={{ display: "block" }}
+        client="ca-pub-3835939635569436"
+        slot="1367382719"
+        format="auto"
+        responsive="true"
+      />
+    </ins>
+    <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+  </>
 );
 
 export default HorizontalBarAd;
